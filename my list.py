464088b3f43cd6557333edccf38ff4a421b78f6a
bahati <bahatifidelis2004@gmail.com>
2024-03-05 18:33:55 +0300

    first commit

---
+++ my list.py
@@ -0,0 +1,12 @@
+my_list=[]
+my_list.append([10,20,30,40])
+print("my appended list is:",my_list)
+my_list.insert(1,15)
+print("my inserted list is:",my_list)
+my_list.extend([50,60,70])
+print("my extended list is:",my_list)
+my_list.pop()
+print("removed list is:",my_list)
+my_list.sort()
+print("sorted list is:",my_list)
+print(my_list.index([30]))
